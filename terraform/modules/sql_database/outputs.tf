2247b0d0e765894a7725d74387a863df49f43475
DVDJNBR <d4v1dbr34u@gmail.com>
2026-02-16 11:14:53 +0100

    fix: resolve backup test failures by adding missing terraform outputs and fixing az cli commands

--- terraform/modules/sql_database/outputs.tf
+++ terraform/modules/sql_database/outputs.tf
@@ -6,6 +6,10 @@ output "server_fqdn" {
   value = azurerm_mssql_server.sql_server.fully_qualified_domain_name
 }
 
+output "server_name" {
+  value = azurerm_mssql_server.sql_server.name
+}
+
 output "database_name" {
   value = azurerm_mssql_database.dwh.name
 }
